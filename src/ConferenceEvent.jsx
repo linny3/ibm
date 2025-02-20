97473d411fdd0166abb5ffc5093c997deb1dd90a
linny3 <ryan.lindberg3@gmail.com>
2025-02-20 11:18:46 -0500

    meal selection

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -13,6 +13,8 @@ const ConferenceEvent = () => {
     const dispatch = useDispatch();
     const remainingAuditoriumQuantity = 3 - venueItems.find(item => item.name === "Auditorium Hall (Capacity:200)").quantity;
     const avTotalCost = calculateTotalCost("av");
+    const mealsItems = useSelector((state) => state.meals);
+    
     
     const handleToggleItems = () => {
         console.log("handleToggleItems called");
@@ -196,10 +198,24 @@ const ConferenceEvent = () => {
                                 </div>
 
                                 <div className="input-container venue_selection">
-
+                                    <label htmlFor="numberOfPeople"><h3>Number of People:</h3></label>
+                                    <input type="number" className="input_boxs5" id="numberOfPeople" value={numberOfPeople}
+                                        onChange={(e) => setNumberOfPeople(parseInt(e.target.value))}
+                                        min="1"/>
                                 </div>
                                 <div className="meal_selection">
-
+                                    {mealsItems.map((item,index) => (
+                                <div className="meal_item" key={index} style={{ padding: 15 }}>
+                                    <div className="inner">
+                                        <input type="checkbox" id={`meal_${index}`}
+                                            checked={ item.selected }
+                                            onChange={() => handleMealSelection(index)}
+                                            />
+                                            <label htmlFor={`meal_${index}`}> {item.name} </label>
+                                    </div>
+                                    <div className="meal_cost">${item.cost}</div>
+                                    </div>
+                                ))}
                                 </div>
                                 <div className="total_cost">Total Cost: </div>
 
